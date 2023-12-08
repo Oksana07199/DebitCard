1b9cd052bd6f5abe77705169a7a0eb673bd01fac
Oksana Paygildina <kazaikina1997@mail.ru>
2023-12-08 13:16:38 +0300

    first commit

---
+++ src/test/java/ru/netolofy/ZayavkaTest.java
@@ -0,0 +1,21 @@
+package ru.netolofy;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+public class ZayavkaTest {
+    @Test
+    void shouldTestWithValidFirstSurnameSecondName() {
+        open("http://localhost:9999");
+        SelenideElement form = $(By.className("form"));
+        form.$("[data-test-id=name] input").setValue("Иванова Оксана");
+        form.$("[data-test-id=phone] input").setValue("+79870000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(By.className("button")).click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
